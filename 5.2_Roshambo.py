6650aa2182a027d54a54d9729ab457aa1f19070d
Nellie Leaverton <54453366+Kitrinos@users.noreply.github.com>
2019-10-03 16:18:34 -0500

    Update 5.2_Roshambo.py

--- 5.2_Roshambo.py
+++ 5.2_Roshambo.py
@@ -12,6 +12,8 @@ When the user quits print a win/loss record
 '''
 
 import random
+u = 0
+quit = "no"
 
 number= [1, 2, 3]
 i = random.randrange(3)
@@ -23,6 +25,58 @@ elif i == 2:
 else:
   print("scissors")
 
+while quit.lower() != "yes" or quit.lower() != "y":
+    b = random.randrange(3)+1
+    quit = input("Do you want to quit? ")
+    if quit.lower() == "yes" or quit.lower() == "y":
+        print("You quit")
+    elif quit.lower() == "no" or quit.lower() == "n":
+        u = input("Hello user! What is your choice? (rock, paper, scissors) ")
+
+        if b == 1:
+            print("rock")
+        elif b == 2:
+            print("paper")
+        else:
+            print("scissors")
+
+        if b == 1:
+            if u.lower() == "rock":
+                print("The bot chose rock! You tie!")
+
+        if b == 1:
+            if u.lower() == "paper":
+                print("The bot chose rock! You Win!")
+
+        if b == 1:
+            if u.lower() == "scissors":
+                print("The bot chose rock! You lose!")
+
+        if b == 2:
+            if u.lower() == "paper":
+                print("The bot chose paper! You got a tie!")
+
+        if b == 2:
+            if u.lower() == "rock":
+                print("The bot chose paper! You lose!")
+
+        if b == 2:
+            if u.lower() == "scissors":
+                print("The bot chose paper! You Win!")
+
+        if b == 3:
+            if u.lower() == "scissors":
+                print("The bot chose scissors! You tie!")
+
+        if b == 3:
+            if u.lower() == "rock":
+                print("The bot chose scissors! You Win!")
+
+        if b == 3:
+            if u.lower() == "paper":
+                print("The bot chose scissors! You lose!")
+    else:
+        print("sorry try again")
 
 
 
